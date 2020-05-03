0a33049e878c7e374599fb9742d242d560b865bb
kiyosumaeda <kiyosu.0407@gmail.com>
2020-05-04 00:30:45 +0900

    initial commit

---
+++ Assets/Scripts/ButtonBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBase : MonoBehaviour {
+
+	Button button;
+
+    // Start is called before the first frame update
+    public void Start() {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(ButtonFunc);
+    }
+
+    public virtual void ButtonFunc() {
+
+    }
+}
---
+++ Assets/Scripts/PluginSampleTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PluginSampleTest : ButtonBase {
+
+	[SerializeField] Text test_text;
+
+	public static readonly string ANDROID_NATIVE_PLUGIN_CLASS = "com.example.unitypluginsamplelibrary.TestPlugin";
+
+    // Start is called before the first frame update
+    new void Start() {
+    	base.Start();
+        test_text.text = "";
+    }
+
+    public override void ButtonFunc() {
+    	using (AndroidJavaClass androidJavaClass = new AndroidJavaClass(ANDROID_NATIVE_PLUGIN_CLASS)) {
+    		Debug.Log("aaaa");
+    		androidJavaClass.CallStatic("Execute");
+    	}
+    }
+
+    public void FromAndroid(string message) {
+    	Debug.Log("bbbb");
+    	test_text.text = message;
+    }
+}
